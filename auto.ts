85cf6da12762c585992d7e6b12ebf51bc76d4d6c
irinamarzioni <imarzioni@devartis.com>
2022-09-19 14:41:40 -0300

    correccion

--- auto.ts
+++ auto.ts
@@ -1,4 +1,4 @@
-class Auto{
+class Auto {
     private modelo: string;
     private marca: string;
     private precio: number;
@@ -8,64 +8,62 @@ class Auto{
 
     //el metodo constructor es publico por defecto.
 
-    constructor(auxModelo:string,auxMarca:string,auxPrecio:number){
+    constructor(auxModelo: string, auxMarca: string, auxPrecio: number) {
         this.modelo = auxModelo;
         this.marca = auxMarca;
         this.precio = auxPrecio;
         this.descuento = false;
-        this.precioFinal = this.precio*1.25;
+        this.precioFinal = this.precio * 1.25;
         this.premium = false;
     }
 
-
-        getDescuento():void{
-            if(this.descuento===true){
-                console.log("Tiene descuento");
-            } else{
-                console.log("No tiene descuento");
-            }
+    public getDescuento(): void {
+        if (this.descuento === true) {
+            console.log("Tiene descuento");
+        } else {
+            console.log("No tiene descuento");
         }
+    }
 
-        setDescuento():void{
-            if(this.descuento===false){
-                this.descuento = true;
-                this.setPrecioBase();
-            }
+    public setDescuento(): void {
+        if (this.descuento === false) {
+            this.descuento = true;
+            this.setPrecioBase();
         }
+    }
 
-        setPrecioBase():void{
-            this.precio = this.precio*0.85;
+    public setPrecioBase(): void {
+        this.precio = this.precio * 0.85;
+    }
+
+    public getPrecioBase(): void {
+        console.log(this.precio);
+    }
+
+    public setPrecioFinalPremium(): void {
+        if (this.premium === true) {
+            this.precioFinal = this.precioFinal * 1.25;
         }
+    }
 
-        getPrecioBase():void{
-            console.log(this.precio); 
+    public getPremium(): void {
+        if (this.premium === true) {
+            console.log("Usted contrato el servicio premium, precio final: " + this.precioFinal);
+        } else {
+            console.log("Usted no contrato el servicio premium, precio final: " + this.precioFinal);
         }
+    }
 
-        setPrecioFinalPremium():void{
-            if(this.premium===true){
-                this.precioFinal = this.precioFinal*1.25;
-            }
-        }
-
-        getPremium():void{
-            if(this.premium===true){
-                console.log("Usted contrato el servicio premium, precio final: " +this.precioFinal);
-            } else {
-                console.log("Usted no contrato el servicio premium, precio final: " +this.precioFinal);
-            }
-        } 
-
-        setPremium():void{
-            console.log("Usted decidio añadir el servicio premium.")
-            this.premium = true;
-            this.setPrecioFinalPremium();
-            console.log("El precio final es: "+this.precioFinal);
-        }
-
+    public setPremium(): void {
+        console.log("Usted decidio añadir el servicio premium.")
+        this.premium = true;
+        this.setPrecioFinalPremium();
+        console.log("El precio final es: " + this.precioFinal);
+    }
 }
 
-let primerAuto: Auto = new Auto("Kombi","Volkswagen",35000);
-let segundoAuto: Auto = new Auto("Nivus","Volkswagen",35000);
+let primerAuto: Auto = new Auto("Kombi", "Volkswagen", 35000);
+let segundoAuto: Auto = new Auto("Nivus", "Volkswagen", 35000);
 
 console.log(primerAuto);
 console.log(segundoAuto);
